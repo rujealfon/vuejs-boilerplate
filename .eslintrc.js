d473346aa803f4e5128a3e00d57be315927f32d8
Ruje Alfon <ruje.alfon@bcstechnology.com.au>
2019-08-07 15:26:39 +0800

    add bootstrap-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
-  globals: {
-    $nuxt: true
-  },
   parserOptions: {
     parser: "babel-eslint"
   }
